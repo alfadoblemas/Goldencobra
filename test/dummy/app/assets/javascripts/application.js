d19da3e3aaddfe815dad77e756b7ff133784a82b
Marco Metz <marco.metz@gmail.com>
2013-10-18 11:09:54 +0200

    GC-16 erste Tests im Application layout: 2 Divs ändern ihren inhalt je nach breite des Fensters. Was noch felht ist das anschließende dynamische laden der fehlenden Inhalte

--- test/dummy/app/assets/javascripts/application.js
+++ test/dummy/app/assets/javascripts/application.js
@@ -10,3 +10,12 @@
 // WARNING: THE FIRST BLANK LINE MARKS THE END OF WHAT'S TO BE PROCESSED, ANY BLANK LINE SHOULD
 // GO AFTER THE REQUIRES BELOW.
 //
+
+$(document).ready(function() {
+	Response.create({
+	  prop: "width",  // "width" "device-width" "height" "device-height" or "device-pixel-ratio"
+	  prefix: "min-width-",  // the prefix(es) for your data attributes (aliases are optional)
+	  breakpoints: [1281,1025,961,641,481,320,0], // min breakpoints (defaults for width/device-width)
+	  lazy: true // optional param - data attr contents lazyload rather than whole page at once
+	});
+});
