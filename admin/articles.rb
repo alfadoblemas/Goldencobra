78f199d8e3347cab1d0d9aad2a5fec866b42099f
Marco Metz <marco.metz@gmail.com>
2013-12-19 14:40:51 +0100

    change article type

--- admin/articles.rb
+++ admin/articles.rb
@@ -237,6 +237,25 @@ ActiveAdmin.register Goldencobra::Article, :as => "Article" do
     redirect_to :action => :show
   end
 
+  member_action :change_articletype, :method => :post do
+    article = Goldencobra::Article.find(params[:id])
+    article.article_type = params[:new_article_type]
+    if article.article_type.include?("Show")
+      unless article.article_type.include?("Default")
+        article_model = article.article_type_form_file.constantize
+        related_article_object = article_model.where(:article_id => article.id).first
+        if related_article_object
+          article.send("#{article.article_type_form_file.downcase}=", related_article_object)
+        else
+          article.send("#{article.article_type_form_file.downcase}=", article_model.new(:article_id => article.id))
+        end
+        article.save
+      end
+    end
+    article.save
+    redirect_to :action => :edit
+  end
+
   member_action :set_page_online_offline do
     article = Goldencobra::Article.find(params[:id])
     if article.active
